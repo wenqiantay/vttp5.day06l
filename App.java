3fd495a1c6679e8d2aa31dfc0dbaf3c62331dc77
wenqiantay <wenqianhello@gmail.com>
2024-10-17 11:00:10 +0800

    commit at 11am

--- App.java
+++ App.java
@@ -1,7 +1,12 @@
+import day06.MyImplementation;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class App {
 
     //compile (complile in the project root folder)
     //javac -d bin --source-path . ./*java
+    //javac -d bin --source-path . ./*java day 06*/.java
 
     //run
     //java -cp bin App
@@ -11,16 +16,26 @@ public class App {
 
     
     public static void main(String[] args) {
-        
-        Thread th = new Thread(new Runnable() {
-        
-            public void run() {
-                for (int i = 0; i < 10; i++) {
-                    System.out.println(Thread.currentThread().getName() + "->" + i);
-                }
-            }
-        
-        });
-         th.start(); //this is for the thread to run
+    
+        MyImplementation my1 = new MyImplementation();
+        MyImplementation my2 = new MyImplementation();
+        MyImplementation my3 = new MyImplementation();
+        MyImplementation my4 = new MyImplementation();
+        MyImplementation my5 = new MyImplementation();
+
+        //execute the tasks using a single thread
+        // ExecutorService es = Executors.newSingleThreadExecutor();
+        // es.execute(my1);
+        // es.execute(my2);
+        // es.shutdown();
+
+        //execute the tasks using a fixed thread pool
+        ExecutorService es1 = Executors.newFixedThreadPool(5);
+        es1.execute(my1);
+        es1.execute(my2);
+        es1.execute(my3);
+        es1.execute(my4);
+        es1.execute(my5);
+        es1.shutdown(); //release the memory
     }
 }
---
+++ day06/Product.java
@@ -0,0 +1,56 @@
+package day06;
+
+public class Product {
+    private long id;
+    private String name;
+    private String description;
+    private String category;
+    
+    public Product(long id, String name, String description, String category) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.category = category;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    @Override
+    public String toString() {
+        return "Product [id=" + id + ", name=" + name + ", description=" + description + ", category=" + category + "]";
+    }
+
+   
+    
+    
+}
